abf39c8a1a43399f6eacb8fcae041a614e3b4c9c
ioan.sendroiu <ioan.sendroiu@uti.ro>
2018-10-11 15:02:42 +0300

    eliminated AppConfig

--- post-construct-example/src/main/java/uti/ro/java/tutorials/App.java
+++ post-construct-example/src/main/java/uti/ro/java/tutorials/App.java
@@ -1,14 +1,18 @@
 package uti.ro.java.tutorials;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("uti.ro.java.tutorials")
 public class App 
 {
     public static void main( String[] args )
     {
 
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(App.class);
 
         Hello hello = context.getBean(Hello.class);
 
--- post-construct-example/src/main/java/uti/ro/java/tutorials/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package uti.ro.java.tutorials;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("uti.ro.java.tutorials")
-public class AppConfig {
-}
